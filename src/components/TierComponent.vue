0fc2eb77a19ca223a9236cfc2d31149af28cd838
Sean Chalmers <schalmers3@gatech.edu>
2023-01-30 17:14:48 -0500

    feat: generate tier list dynamically

---
+++ src/components/TierComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="tier">
+    <div class="tier-left">
+      <slot name="left"></slot>
+    </div>
+    <div class="tier-letter" :style="{'background-color': backgroundColor}">{{ name }}</div>
+    <div class="tier-right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TierComponent",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    letterStyle: {
+      type: Object,
+      required: false,
+    },
+  },
+computed: {
+    backgroundColor() {
+      if (this.letterStyle && this.letterStyle["type"] === "color" && this.letterStyle["background-color"]) {
+        return this.letterStyle["background-color"];
+      }
+      return "black";
+    },
+  },
+}
+</script>
+
+<style scoped>
+.tier-letter {
+  width: 65px;
+  aspect-ratio: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  margin: 4px;
+}
+</style>
--- src/components/TierList.vue
+++ src/components/TierList.vue
@@ -6,85 +6,128 @@
           <h1>The Movie Night Tier List</h1>
         </div>
         <div class="tier-letter" id="valhalla">Valhalla</div>
-        <div class="tier-right">
-          <img class="tier-poster" src="https://a.ltrbxd.com/resized/film-poster/2/4/2/2/8/5/242285-puss-in-boots-the-last-wish-0-500-0-750-crop.jpg?v=9e9109c5cd" alt="puss in boots"/>
-        </div>
+        <img class="tier-poster"
+             src="https://a.ltrbxd.com/resized/film-poster/2/4/2/2/8/5/242285-puss-in-boots-the-last-wish-0-500-0-750-crop.jpg?v=9e9109c5cd"
+             alt="puss in boots"/>
       </div>
-      <div class="tier-letter" id="s">S</div>
-      <div class="tier-letter" id="a">A</div>
-      <div class="tier-letter" id="b">B</div>
-      <div class="tier-letter" id="c">C</div>
-      <div class="tier-letter" id="d">D</div>
-      <div class="tier-letter" id="e">E</div>
-      <div class="tier-letter" id="f">F</div>
-      <div class="tier-letter" id="shadowrealm">Shadow Realm</div>
-      <div class="tier-letter" id="chips">CHIPS</div>
+      <TierComponent v-for="tier in tiers" :name="tier.title" :id="tier.id" :key="tier.id" :letter-style="tier.style">
+        <template #left>
+          Test Left
+        </template>
+        <template #right>
+          Test Right
+        </template>
+      </TierComponent>
     </div>
   </div>
 </template>
 
 <script>
+import TierComponent from "@/components/TierComponent.vue";
+
 export default {
-  name: "TierList"
+  name: "TierList",
+  components: {TierComponent},
+  data() {
+    return {
+      tiers: [
+        {
+          id: "valhalla",
+          title: "Valhalla",
+          style: {
+            type: "color",
+            "background-color": "#00ff9d"
+          },
+          leftContent: {
+            type: "html",
+            value: "<div>test</div>"
+          }
+        },
+        {
+          id: "s",
+          title: "S",
+          style: {
+            type: "color",
+            "background-color": "#ff7f7f"
+          }
+        },
+        {
+          id: "a",
+          title: "A",
+          style: {
+            type: "color",
+            "background-color": "#febe7e"
+          }
+        },
+        {
+          id: "b",
+          title: "B",
+          style: {
+            type: "color",
+            "background-color": "#ffff7f"
+          }
+        },
+        {
+          id: "c",
+          title: "C",
+          style: {
+            type: "color",
+            "background-color": "#7fff7f"
+          }
+        },
+        {
+          id: "d",
+          title: "D",
+          style: {
+            type: "color",
+            "background-color": "#7fbfff"
+          }
+        },
+        {
+          id: "e",
+          title: "E",
+          style: {
+            type: "color",
+            "background-color": "#7f7ffe"
+          }
+        },
+        {
+          id: "f",
+          title: "F",
+          style: {
+            type: "color",
+            "background-color": "#ff7fff"
+          }
+        },
+        {
+          id: "shadowrealm",
+          title: "Shadow Realm",
+          style: {
+            type: "color",
+            "background-color": "#400d59"
+          }
+        },
+        {
+          id: "chips",
+          title: "CHIPS",
+          style: {
+            type: "color",
+            "background-color": "white"
+          }
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
-.tier-letter {
-  width: 65px;
-  aspect-ratio: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: bold;
-  margin: 4px;
-}
+
 
 h1 {
   font-family: "Comic Sans MS", sans-serif;
   font-size: 40px;
   color: #ffffff;
-}
-
-#s {
-  background-color: #ff7f7f;
-}
-
-#a {
-  background-color: #febe7e;
-}
-
-#b {
-  background-color: #ffff7f;
-}
-
-#c {
-  background-color: #7fff7f;
-}
-
-#d {
-  background-color: #7fbfff;
-}
-
-#e {
-  background-color: #7f7ffe;
-}
-
-#f {
-  background-color: #ff7fff;
-}
-
-#shadowrealm {
-  color: white;
-  background-color: #400d59;
-}
-
-#chips {
-  background-color: white;
-}
-
-#valhalla {
-  background-color: #00ffa6;
 }
 
 .tier {
