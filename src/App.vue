30f8d1e689ba493bc2b9f9dfacf1f4fb4bf9e90d
Sean Chalmers <schalmers3@gatech.edu>
2023-01-29 23:42:42 -0500

    feat: setup tier list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TierList/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TierList from "@/components/TierList.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TierList,
   }
 }
 </script>
---
+++ src/components/TierList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="tier-list">
+    <h1>Tier List</h1>
+    <div class="tiers-letters">
+      <div class="tier-letter" id="valhalla">Valhalla</div>
+      <div class="tier-letter" id="s">S</div>
+      <div class="tier-letter" id="a">A</div>
+      <div class="tier-letter" id="b">B</div>
+      <div class="tier-letter" id="c">C</div>
+      <div class="tier-letter" id="d">D</div>
+      <div class="tier-letter" id="e">E</div>
+      <div class="tier-letter" id="f">F</div>
+      <div class="tier-letter" id="shadowrealm">Shadow Realm</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TierList"
+}
+</script>
+
+<style scoped>
+
+</style>
