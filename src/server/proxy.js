ca212059ddf3e2351450f00379960517c015bb1f
Sean Chalmers <schalmers3@gatech.edu>
2024-04-06 15:26:17 -0400

    feat: improve image load time

--- src/server/proxy.js
+++ src/server/proxy.js
@@ -7,6 +7,58 @@ const https = require('https');
 const app = express();
 const port = 3008; // You can choose any available port
 
+// fetch every image in tierlist-cached.json
+const fs = require('fs');
+const path = require('path');
+
+const cachedTierlistPath = path.join(__dirname, 'assets', 'tierlist-cached.json');
+const cachePath = path.join(__dirname, 'images');
+const tierlist = JSON.parse(fs.readFileSync(cachedTierlistPath));
+const imageUrls = [];
+
+const BASE_URL = 'http://localhost:' + port;
+
+const addImagesToList = (side) => {
+    // side is an object with keys that correspond to each tier
+    for (const tier of Object.values(side)) {
+        for (const movie of tier) {
+            imageUrls.push(movie.imageUrl);
+        }
+    }
+}
+
+addImagesToList(tierlist.left);
+addImagesToList(tierlist.right);
+
+// fetch and save images
+const fetchAndSaveImage = async (url) => {
+    try {
+        const response = await axios.get(
+            `${BASE_URL}/image_proxy?url=${encodeURIComponent(url)}`,
+            {
+                responseType: 'arraybuffer',
+                httpsAgent: new https.Agent({rejectUnauthorized: false})
+            }
+        );
+        // remove query parameters from the URL
+        const filename = url.split('/').pop().split('?')[0];
+        console.log(`Saving ${filename}`);
+        fs.writeFileSync(path.join(cachePath, filename), response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const fetchAndSaveImages = async () => {
+    for (const url of imageUrls) {
+        await fetchAndSaveImage(url);
+    }
+}
+
+fetchAndSaveImages().then(() => {
+    console.log('Images fetched and saved');
+});
+
 app.use(cors());
 app.use(express.json());
 
@@ -32,6 +84,16 @@ app.get('/image_proxy', async (req, res) => {
         return;
     }
     try {
+        // check if image is already cached
+        const filename = req.query.url.split('/').pop().split('?')[0];
+        const cachedImagePath = path.join(cachePath, filename);
+        if (fs.existsSync(cachedImagePath)) {
+            console.log(`Serving cached image ${filename}`);
+            res.set('Content-Type', 'image/jpeg');
+            res.set('Cache-Control', 'public, max-age=31536000');
+            res.sendFile(cachedImagePath);
+            return;
+        }
         const response = await axios.get(
             req.query.url,
             {
